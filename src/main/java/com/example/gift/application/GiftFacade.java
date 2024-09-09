59cf361d9d32bcfb757283e9ddb202c887d7594f
limminjeong98 <limminjeong98@naver.com>
2024-09-09 21:05:12 +0900

    선물하기 기능 구현

--- src/main/java/com/example/gift/application/GiftFacade.java
+++ src/main/java/com/example/gift/application/GiftFacade.java
@@ -1,5 +1,8 @@
 package com.example.gift.application;
 
+import com.example.gift.domain.gift.GiftCommand;
+import com.example.gift.domain.gift.GiftInfo;
+import com.example.gift.domain.gift.GiftService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GiftFacade {
     private final GiftService giftService;
+
+    public GiftInfo getOrder(String giftToken) {
+        log.info("getOrder giftToken = {}", giftToken);
+        return giftService.getGiftInfo(giftToken);
+    }
+
+    public GiftInfo registerOrder(GiftCommand.Register command) {
+        var giftInfo = giftService.registerOrder(command);
+        log.info("registerOrder orderToken = {}", giftInfo);
+        return giftInfo;
+    }
+
+    public void requestPaymentProcessing(String giftToken) {
+        giftService.requestPaymentProcessing(giftToken);
+    }
+
+    public void completePayment(String orderToken) {
+        giftService.completePayment(orderToken);
+    }
+
+    public void acceptGift(GiftCommand.Accept request) {
+        giftService.acceptGift(request);
+    }
 }
---
+++ src/main/java/com/example/gift/infrastructure/gift/GiftRepository.java
@@ -0,0 +1,13 @@
+package com.example.gift.infrastructure.gift;
+
+import com.example.gift.domain.gift.Gift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GiftRepository extends JpaRepository<Gift, Long> {
+
+    Optional<Gift> findByGiftToken(String giftToken);
+
+    Optional<Gift> findByOrderToken(String orderToken);
+}
